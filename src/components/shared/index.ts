e1a1bd0ba2da12edfd41e0c542d36a6c108e7abb
bekbaevic <iseytmuratov77@gmail.com>
2025-01-18 01:26:57 +0500

    fa

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,5 +2,4 @@ export { Container } from "./Container";
 export { Header } from "./Header";
 export { Footer } from "./Footer";
 export { Logo } from "./Logo";
-export { HeaderMenu } from "./HeaderMenu";
 export { About } from "./About";
